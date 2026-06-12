c9b3eca17c3fdbbf77338cfadf9ea4cc447a1e4a
ali-kkkcloud <alimohdcoder@gmail.com>
2026-06-12 06:37:10 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+export default function Home() {
+  const router = useRouter()
+  useEffect(() => { router.replace('/login') }, [])
+  return <div style={{display:'flex',alignItems:'center',justifyContent:'center',minHeight:'100vh'}}><div className="spinner"></div></div>
+}
