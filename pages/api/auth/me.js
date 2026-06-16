586fae2d9b7f4aac25b6f4982a3ceb96b87b3528
ali-kkkcloud <alimohdcoder@gmail.com>
2026-06-16 09:07:25 +0530

    Update me.js

--- pages/api/auth/me.js
+++ pages/api/auth/me.js
@@ -1,6 +1,6 @@
 import { getUserFromReq } from '../../../lib/auth'
 export default function handler(req, res) {
   const user = getUserFromReq(req)
-  if (!user) return res.status(401).json({ user: null })
+  if (!user) return res.status(401).json({ user:null })
   return res.status(200).json({ user })
 }
